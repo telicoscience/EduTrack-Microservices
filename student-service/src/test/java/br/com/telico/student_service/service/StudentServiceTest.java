38cf67e976880e92434bc9eb507f97dc91dbb6dd
telicoscience <telico@yahoo.com.br>
2026-05-27 16:48:20 -0300

    Add student service unit tests

--- student-service/src/test/java/br/com/telico/student_service/service/StudentServiceTest.java
+++ student-service/src/test/java/br/com/telico/student_service/service/StudentServiceTest.java
@@ -19,4 +19,35 @@ public class StudentServiceTest {
         assertEquals(2, students.size());
         assertEquals("João", students.get(0).getName());
     }
+
+    @Test
+    void shouldReturnStudentById() {
+        StudentService service = new StudentService();
+
+        Student student = service.findById(7L);
+        assertEquals(7L, student.getId());
+        assertEquals("João", student.getName());
+
+    }
+
+    @Test
+    void shouldSearchStudentByName() {
+        StudentService service  = new StudentService();
+
+        List<Student> students = service.searchByName("Ana");
+
+        assertEquals(1, students.size());
+        assertEquals("Ana", students.get(0).getName());
+
+    }
+
+    @Test
+    void shouldCreateStudent() {
+        StudentService service = new StudentService();
+        Student student = service.create("Carlos");
+        
+        assertEquals(3L, student.getId());
+        assertEquals("Carlos", student.getName());
+
+    }
 }
