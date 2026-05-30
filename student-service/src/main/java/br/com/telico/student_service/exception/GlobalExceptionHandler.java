4ad9837307605a479defc0ae1ad75aed413d7ff7
telicoscience <telico@yahoo.com.br>
2026-05-30 14:31:25 -0300

    fix issue on GlobalExceptionHandler

--- student-service/src/main/java/br/com/telico/student_service/exception/GlobalExceptionHandler.java
+++ student-service/src/main/java/br/com/telico/student_service/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import br.com.telico.student_service.exception.StudentNotFoundException;
 
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(StudentNotFoundException.class)
