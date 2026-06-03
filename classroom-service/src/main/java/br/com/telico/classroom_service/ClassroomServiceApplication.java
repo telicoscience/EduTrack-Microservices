fc07691d1a55ad379260c8a6a73e46f682200dff
telicoscience <telico@yahoo.com.br>
2026-06-03 20:02:09 -0300

    Initialize classroom service

---
+++ classroom-service/src/main/java/br/com/telico/classroom_service/ClassroomServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.telico.classroom_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassroomServiceApplication.class, args);
+    }
+
+}
